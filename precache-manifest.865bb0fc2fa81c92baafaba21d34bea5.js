091db38568177ac8d00f0d1ce379d393522229f8
zhenekv <zhenekvv@gmail.com>
2019-12-26 00:26:13 +0300

    Updates

---
+++ precache-manifest.865bb0fc2fa81c92baafaba21d34bea5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8f4ff2d2254334977155bb66be2420",
+    "url": "/vk-apps-get-money/index.html"
+  },
+  {
+    "revision": "5f345d3b8f607f914e3b",
+    "url": "/vk-apps-get-money/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "066cb3d75e6cab1d9827",
+    "url": "/vk-apps-get-money/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "5f345d3b8f607f914e3b",
+    "url": "/vk-apps-get-money/static/js/2.0e39e9a5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vk-apps-get-money/static/js/2.0e39e9a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "066cb3d75e6cab1d9827",
+    "url": "/vk-apps-get-money/static/js/main.62cef779.chunk.js"
+  },
+  {
+    "revision": "2eddc77c18c380c2b736",
+    "url": "/vk-apps-get-money/static/js/runtime-main.ed586e9d.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-get-money/static/media/persik.4e1ec840.png"
+  }
+]);
